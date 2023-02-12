b6c9bd360965f9d9831279b0ff0347a8a94cb820
coldfrontlich <coldfrontlich@gmail.com>
2023-02-13 01:26:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let stack = [];
+  stack.push(str.charAt(0));
+  for (let i = 1; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str.charAt(i) === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]) {
+        stack.pop();
+        break;
+      }
+      if (str.charAt(i) === bracketsConfig[j][0] || str.charAt(i) === bracketsConfig[j][1]) {
+        stack.push(str.charAt(i));
+        break;
+      } 
+    }
+  }
+  return (stack.length === 0);
+} 
